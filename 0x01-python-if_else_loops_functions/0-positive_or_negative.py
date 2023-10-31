6844d775b2f4b7bd5926069d9fc0fe3bfe0759c5
0Fly98 <lundujosh97@gmail.com>
2023-10-31 08:56:05 -0700

    python

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-
-# Define a dictionary to map number ranges to their descriptions
 descriptions = {
-    number > 0: "{} is positive".format(number),
-    number == 0: "{} is zero".format(number),
-    number < 0: "{} is negative".format(number)
+    number > 0: f"{number:d} is positive",
+    number == 0: f"{number:d} is zero",
+    number < 0: f"{number:d} is negative"
 }
-
-# Print the description for the number
 print(descriptions[True])
