c4eca7e660ca834f90b8b304abed65b68ba577b3
joshua lundu <122810726+0Fly98@users.noreply.github.com>
2023-10-31 21:22:20 +0200

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for u in range(97, 123):
-    if chr(u) == 'q' or chr(u) == 'e':
-        continue
-    print("{}".format(chr(u)), end="")
+print("".join([chr(u) for u in range(97, 123) if chr(u) not in "qe"]))
