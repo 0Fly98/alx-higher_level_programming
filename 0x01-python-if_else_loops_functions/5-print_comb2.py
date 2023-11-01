514cef8d0064271f354034c5f6339475b7272a0d
joshua lundu <122810726+0Fly98@users.noreply.github.com>
2023-11-01 20:23:51 +0200

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for u in range(0, 100):
+
+for u in range(100):
     if u == 99:
         print(99)
-        break
-    print("{:02d}, ".format(u), end=""
+    else:
+        print("{:02d}, ".format(u), end='')
