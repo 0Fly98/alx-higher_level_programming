d18b7ebab94fc140006c2c6107970150f13ea5f1
0Fly98 <lundujosh97@gmail.com>
2023-11-02 09:03:27 -0700

    python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-if _name_ == "_main_:
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-
-    operations = {
-    'addition': add,
-    'subtraction': sub,
-    'multiplication': mul,
-    'division': div
-    }
-
     a = 10
     b = 5
-
-    for operation, function in operations.items():
-    print("The result of the {} of {:d} and {:d} is {:d}" .format(operation, a, b, function(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
