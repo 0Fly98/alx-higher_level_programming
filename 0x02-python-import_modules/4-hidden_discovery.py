5eadd51194676ce6bce670aa9ce92711cfb2b822
0Fly98 <lundujosh97@gmail.com>
2023-11-03 09:16:57 -0700

    presssure

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import hidden_4
-    # Print sorted name from directory
-    for name in sorted(dir(hidden_4)):
-        # print only names that do not start with __
-        if name[:2] != '__':
-            print("{}".format(name))
+    """adds infinite no's on cLD arg"""
+    import sys
+
+    argv = sys.argv
+    y = len(argv) - 1
+    sum = 0
+    if y == 0:
+        print(sum)
+    else:
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print(sum)
