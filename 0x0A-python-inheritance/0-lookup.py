226b14f08a8b44ee8fa066838dc9c5004a0a562e
0Fly98 <lundujosh97@gmail.com>
2023-12-05 09:27:55 -0800

    My list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""To lookup list of avaliable attr and methods"""
+
 def lookup(obj):
-    return dir(obj)
+    """returns available attr and methods in object"""
+    return (dir(obj))
