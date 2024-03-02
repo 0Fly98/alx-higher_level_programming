9c414525ae756fef92398404c93b2f9f8a7b7353
0Fly98 <lundujosh97@gmail.com>
2024-03-02 00:34:36 -0800

    pycode

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
-# http://21701fe1a6fd.19.hbtn-cod.io:5000/search_user
 
+"""
+Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
 
-if __name__ == "__main__":
-    if len(argv) == 2:
-        url = "http://0.0.0.0:5000/search_user"
-        payload = {}
-
-        if argv[1]:
-            payload['q'] = argv[1]
-        else:
-            payload['q'] = ""
-
-        res = requests.post(url, data=payload)
-        res.raise_for_status()
-
-        try:
-            obj = res.json()
-            try:
-                _id = "[{}] ".format(obj['id'])
-                name = "{}".format(obj['name'])
-                print(_id + name)
-            except:
-                print('No result')
-        except:
-            print('Not a valid JSON')
-    else:
-        print('No result')
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        ar = sys.argv[1]
+    except Exception:
+        ar = ""
+    q = {"q": ar}
+    r = requests.post(url, data=q)
+    try:
+        result = r.json()
+    except Exception:
+        print("Not a valid JSON")
+        exit()
+    try:
+        print("[{}] {}".format(result['id'], result['name']))
+    except Exception:
+        print("No result")
