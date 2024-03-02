a11a60aa1f6b901c70fff13334dae36de440c0d0
0Fly98 <lundujosh97@gmail.com>
2024-03-02 00:11:21 -0800

    python3

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import requests
+"""
+same as 0-hbtn_status with requests model
+"""
 
-if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    res = requests.get(url)
-    if res.status_code == 200:
-        print('Body response:')
-        print('\t- type: {}'.format(type(str(res))))
-        print('\t- content: {}'.format(res.text))
+if __name__ == '__main__':
+    import requests
+    html = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
