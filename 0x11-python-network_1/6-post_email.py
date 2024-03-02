773f6f3f31ebc180b3635e3fd0744613cb31e5e2
0Fly98 <lundujosh97@gmail.com>
2024-03-02 00:26:09 -0800

    pyhon3

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
+"""
+same as 2-post_email with requests models
+"""
 
-
-if __name__ == "__main__":
-    url = argv[1]
-    email = argv[2]
-    payload = {'email': email}
-
-    res = requests.post(url, data=payload)
-    print(res.text)
+if __name__ == '__main__':
+    import requests
+    import sys
+    para = {"email": sys.argv[2]}
+    r = requests.post(sys.argv[1], data=para)
+    print(r.text)
