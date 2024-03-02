96cec7694d8bdd108cd9b65e7325c23e23fbc3b8
0Fly98 <lundujosh97@gmail.com>
2024-03-01 23:57:52 -0800

    python3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-from sys import argv
+"""
+sends a POST request to the passed URL with the
+email as a parameter, and displays the body of the response
+"""
 
-if __name__ == "__main__":
-
-    url = argv[1]
-    values = {'email': argv[2]}
-    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-    headers = {'User-Agent': user_agent}
-
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-
-    req = urllib.request.Request(url, data, headers)
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode("ascii")
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        page = response.read()
-        page = page.decode('utf-8')
-    print(page)
+        info = response.read()
+    print(info.decode("ascii"))
